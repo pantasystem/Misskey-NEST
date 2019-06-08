580cc3ca648c20b379561bc18c81a2cf609e025f
kinoshita0623 <kinoshita062323@gmail.com>
2019-06-08 18:48:13 +0900

    SVGタイプのカスタム絵文字にも対応

--- app/src/main/java/org/panta/misskey_nest/emoji/CustomEmoji.kt
+++ app/src/main/java/org/panta/misskey_nest/emoji/CustomEmoji.kt
@@ -3,12 +3,16 @@ package org.panta.misskey_nest.emoji
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ImageSpan
-import android.util.Log
 import android.widget.TextView
+import com.caverock.androidsvg.SVG
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 
 class CustomEmoji(private val context: Context, private val emojiFileList: List<File>){
     private val emojiMap = emojiFileList.map{
@@ -24,27 +28,60 @@ class CustomEmoji(private val context: Context, private val emojiFileList: List<
             if(emojiFile == null){
                 spannable.append(t)
             }else{
-                val bitmap = BitmapFactory.decodeFile(emojiFile.path)
-                val tmpSize = if(bitmap.width < 50){
-                    size ?: 80
+                val bitmap = if(emojiFile.path.endsWith(".svg")){
+                    getBitmapFromSVG(emojiFile, textView.textSize.toInt(), textView.textSize.toInt())
                 }else{
-                    size
-                }
-
-                val showBitmap = if(tmpSize == null){
-                    bitmap
-                }else{
-                    val scale = tmpSize / bitmap.width.toDouble()
-                    Bitmap.createScaledBitmap(bitmap, (bitmap.width * scale).toInt(), (bitmap.height * scale).toInt(), true)
+                    resizeBitmap(BitmapFactory.decodeFile(emojiFile.path), textView.textSize.toInt())
                 }
 
 
-                val imageSpan = ImageSpan(context, showBitmap)
+
+                val imageSpan = ImageSpan(context, bitmap)
                 val start = spannable.length
                 spannable.append(t)
                 spannable.setSpan(imageSpan, start, start + t.length , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
             }
         }
         textView.text = spannable
+    }
+
+    private fun getBitmapFromSVG(file: File, width: Int, height: Int): Bitmap{
+        if( ! file.path.endsWith(".svg") ) throw IllegalAccessException("This file is not svg file. You must use svg file")
+
+        val stream = BufferedReader(InputStreamReader(file.inputStream()))
+        val builder = StringBuilder()
+        while(true){
+            val next: String? = stream.readLine()
+            if(next == null){
+                break
+            }else{
+                builder.append(next)
+            }
+        }
+
+        val svg = SVG.getFromString(builder.toString())
+
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        svg.renderToCanvas(canvas)
+
+        return bitmap
+
+    }
+
+    private fun resizeBitmap(bitmap: Bitmap, size: Int?): Bitmap{
+        val tmpSize = if(bitmap.width < 50){
+            size ?: 80
+        }else{
+            size
+        }
+
+        val showBitmap = if(tmpSize == null){
+            bitmap
+        }else{
+            val scale = tmpSize / bitmap.width.toDouble()
+            Bitmap.createScaledBitmap(bitmap, (bitmap.width * scale).toInt(), (bitmap.height * scale).toInt(), true)
+        }
+        return showBitmap
     }
 }
--- app/src/main/java/org/panta/misskey_nest/service/EmojiDownloadService.kt
+++ app/src/main/java/org/panta/misskey_nest/service/EmojiDownloadService.kt
@@ -3,10 +3,16 @@ package org.panta.misskey_nest.service
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.res.XmlResourceParser
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.VectorDrawable
 import android.os.IBinder
+import android.support.graphics.drawable.VectorDrawableCompat
 import android.util.Log
+import android.util.Xml
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.coroutines.GlobalScope
@@ -17,7 +23,11 @@ import org.panta.misskey_nest.network.HttpsConnection
 import org.panta.misskey_nest.network.OkHttpConnection
 import org.panta.misskey_nest.repository.PersonalRepository
 import org.panta.misskey_nest.storage.SharedPreferenceOperator
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedInputStream
+import java.io.StringReader
+import java.lang.StringBuilder
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
@@ -52,9 +62,9 @@ class EmojiDownloadService : Service() {
 
                         //val fileName = it.name.split("/").last()
                         if(it.type.endsWith("svg+xml")){
-                            //saveSvg(it.url!!, "${it.name}.svg")
+                            saveSvg(it.url!!, it.name)
                         }else{
-                            saveImage(it.url!!, "${it.name}.png")
+                            saveImage(it.url!!, it.name)
                         }
                     }
                 }
@@ -73,7 +83,7 @@ class EmojiDownloadService : Service() {
                 val inputStream  = BufferedInputStream(connection.inputStream)
                 val bitmap = BitmapFactory.decodeStream(inputStream)
 
-                val fos = applicationContext.openFileOutput(fileName, Context.MODE_PRIVATE)
+                val fos = applicationContext.openFileOutput("$fileName.png", Context.MODE_PRIVATE)
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
                 /*var len = inputStream.read()
                 while(len != -1){
@@ -95,7 +105,32 @@ class EmojiDownloadService : Service() {
 
                 val connection = URL(url).openConnection() as HttpsURLConnection
                 connection.connect()
-                //val inputStream =
+                val inputStream = BufferedInputStream(connection.inputStream)
+
+                /*val xppf = XmlPullParserFactory.newInstance()
+                val parser = xppf.newPullParser()
+                parser.setInput(inputStream, null)
+
+                val drawable = VectorDrawableCompat.createFromXml(applicationContext.resources, parser)*/
+                /*SVGParser
+                val svg = SVGParser.getSVGFromInputStream(inputStream)
+                val drawable: Drawable = SVGParser.createPictureDrawable()
+                val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+                val canvas = Canvas(bitmap)
+                drawable.setBounds(0,0, canvas.width, canvas.height)
+                drawable.draw(canvas)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, applicationContext.openFileOutput("$fileName.png", Context.MODE_PRIVATE))*/
+                val ops = applicationContext.openFileOutput("$fileName.svg", Context.MODE_PRIVATE)
+                var next = inputStream.read()
+                while(next != -1){
+                    ops.write(next)
+                    next = inputStream.read()
+                }
+                ops.flush()
+                ops.close()
+
+                Log.d(this.toString(), "SVG保存成功 $fileName")
+
             }catch(e: Exception){
                 Log.d(this.toString(), "保存中にエラー $url, $fileName", e)
             }
