5e972eab024e675847d510e9f7d4ec5ba934de62
kinoshita0623 <kinoshita062323@gmail.com>
2019-06-09 00:02:31 +0900

    SpannableCacheにキャッシュの上限を設けた

--- app/src/main/java/org/panta/misskey_nest/util/SpannableStringBuilderCache.kt
+++ app/src/main/java/org/panta/misskey_nest/util/SpannableStringBuilderCache.kt
@@ -1,11 +1,14 @@
 package org.panta.misskey_nest.util
 
 import android.text.SpannableStringBuilder
+import java.util.*
+import kotlin.collections.HashMap
 
 class SpannableStringBuilderCache {
 
     companion object{
         private val spannableMap = HashMap<String, SpannableStringBuilder>()
+        private val mapLogDeque = ArrayDeque<String>()
     }
 
     fun get(text: String): SpannableStringBuilder?{
@@ -16,7 +19,25 @@ class SpannableStringBuilderCache {
 
     fun put(text: String, spannable: SpannableStringBuilder){
         synchronized(spannableMap){
-            spannableMap[text] = spannable
+            val cache = spannableMap[text]
+            if(cache == null){
+                spannableMap[text] = spannable
+                synchronized(mapLogDeque){
+                    mapLogDeque.add(text)
+                }
+            }else{
+                return
+            }
+
         }
+
+        synchronized(spannableMap){
+            if(mapLogDeque.size > 20){
+                val key = mapLogDeque.removeFirst()
+                spannableMap.remove(key)
+            }
+        }
+
+
     }
 }
