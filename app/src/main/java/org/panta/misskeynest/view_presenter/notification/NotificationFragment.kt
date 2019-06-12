8ca950ef5ee9ab08f82a7a339f5a5a61f9cbf466
kinoshita0623 <kinoshita062323@gmail.com>
2019-06-12 14:00:10 +0900

    Notificationの一部をボタン、Viewをクリックしたら動作するようにした TODO 他のコードからコピーしてきた部分が多いので共通化する

--- app/src/main/java/org/panta/misskeynest/view_presenter/notification/NotificationFragment.kt
+++ app/src/main/java/org/panta/misskeynest/view_presenter/notification/NotificationFragment.kt
@@ -1,5 +1,8 @@
 package org.panta.misskeynest.view_presenter.notification
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
@@ -13,9 +16,21 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_notification.*
 import org.panta.misskeynest.R
 import org.panta.misskeynest.adapter.NotificationAdapter
+import org.panta.misskeynest.constant.NoteType
 import org.panta.misskeynest.entity.ConnectionProperty
+import org.panta.misskeynest.entity.FileProperty
+import org.panta.misskeynest.entity.Note
+import org.panta.misskeynest.entity.User
+import org.panta.misskeynest.interfaces.INoteClickListener
 import org.panta.misskeynest.interfaces.IOperationAdapter
+import org.panta.misskeynest.interfaces.IUserClickListener
+import org.panta.misskeynest.util.copyToClipboad
+import org.panta.misskeynest.view_data.NoteViewData
 import org.panta.misskeynest.view_data.NotificationViewData
+import org.panta.misskeynest.view_presenter.image_viewer.ImageViewerActivity
+import org.panta.misskeynest.view_presenter.note_description.NoteDescriptionActivity
+import org.panta.misskeynest.view_presenter.note_editor.EditNoteActivity
+import org.panta.misskeynest.view_presenter.user.UserActivity
 
 class NotificationFragment : Fragment(), NotificationContract.View{
 
@@ -82,7 +97,10 @@ class NotificationFragment : Fragment(), NotificationContract.View{
         activity?.runOnUiThread{
 
             notification_view?.layoutManager = mLayoutManager
-            val adapter = org.panta.misskeynest.adapter.NotificationAdapter(list)
+            val adapter = NotificationAdapter(list)
+            adapter.noteClickListener = noteClickListener
+            adapter.userClickListener = userClickListener
+
             notification_view?.adapter = adapter
             notificationAdapter = adapter
             notification_view?.addOnScrollListener(listener)
@@ -112,4 +130,72 @@ class NotificationFragment : Fragment(), NotificationContract.View{
             }
         }
     }
+
+
+    //TODO TimelineFragmentからコピーしてきた必ず共通化する
+    private val userClickListener = object: IUserClickListener {
+        override fun onClickedUser(user: User) {
+            UserActivity.startActivity(context!!, user)
+        }
+    }
+
+    private val noteClickListener = object: INoteClickListener {
+        override fun onNoteClicked(targetId: String?, note: Note?) {
+            Log.d("TimelineFragment", "Noteをクリックした")
+            val intent = Intent(context, NoteDescriptionActivity::class.java)
+            intent.putExtra(NoteDescriptionActivity.NOTE_DESCRIPTION_NOTE_PROPERTY, note)
+            startActivity(intent)
+        }
+        override fun onReplyButtonClicked(targetId: String?, note: Note?) {
+            EditNoteActivity.startActivity(context!!, targetId, NoteType.REPLY)
+        }
+
+        override fun onReactionClicked(targetId: String?, note: Note?, viewData: NoteViewData,reactionType: String?) {
+            //mPresenter?.setReactionSelectedState(targetId, note, viewData, reactionType)
+        }
+
+        override fun onReNoteButtonClicked(targetId: String?, note: Note?) {
+            EditNoteActivity.startActivity(context!!, targetId, NoteType.RE_NOTE)
+        }
+
+        override fun onDescriptionButtonClicked(targetId: String?, note: Note?) {
+            val item = arrayOf<CharSequence>("内容をコピー", "リンクをコピー", "お気に入り", "ウォッチ", "デバッグ（開発者向け）")
+
+            AlertDialog.Builder(activity).apply{
+                setTitle("詳細")
+                setItems(item){ dialog, which->
+                    when(which){
+                        0 -> copyToClipboad(context, note?.text.toString())
+                        1 -> copyToClipboad(context, note?.url.toString())
+                        2,3 -> Toast.makeText(context, "未実装ですごめんなさい", Toast.LENGTH_SHORT)
+                        4 ->{
+                            AlertDialog.Builder(activity).apply{
+                                if(note is Note){
+                                    val noteString = note.toString().replace(",","\n")
+                                    setMessage(noteString)
+                                }
+                                setPositiveButton(android.R.string.ok){i ,b->
+                                }
+                            }.show()
+                        }
+                    }
+
+                }
+            }.show()
+
+
+        }
+        override fun onImageClicked(clickedIndex: Int, clickedImageUrlCollection: Array<String>) {
+            ImageViewerActivity.startActivity(context, clickedImageUrlCollection, clickedIndex)
+        }
+
+        override fun onMediaPlayClicked(fileProperty: FileProperty) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(fileProperty.url)))
+        }
+
+
+    }
+
+
+
 }
