e08d98caeb760599505a4c68e6c8f8fe2e6b1306
kinoshita0623 <kinoshita062323@gmail.com>
2019-06-15 10:25:59 +0900

    NoteViewHolderを抽象クラスにし、レイアウトを容易に変更しやすくした

--- app/src/main/java/org/panta/misskeynest/adapter/DetailNoteViewHolder.kt
+++ app/src/main/java/org/panta/misskeynest/adapter/DetailNoteViewHolder.kt
@@ -1,9 +1,42 @@
 package org.panta.misskeynest.adapter
 
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.item_detailed_note.view.*
+import org.panta.misskeynest.interfaces.ITimeFormat
+import org.panta.misskeynest.util.ElapsedTimeFormatter
 
-class DetailNoteViewHolder(itemView: View) : NoteViewHolder(itemView, LinearLayoutManager(itemView.context)){
+class DetailNoteViewHolder(itemView: View) : AbsNoteViewHolder(itemView){
 
+    override val timelineItem: RelativeLayout = itemView.item_view
+    override val whoReactionUserLink: Button = itemView.who_reaction_user_link
+    override val userIcon: ImageView = itemView.user_icon
+    override val userName: TextView = itemView.user_name
+    override val userId: TextView = itemView.user_id
+    override val noteText: TextView = itemView.note_text
+
+    override val subNote: RelativeLayout = itemView.sub_note
+    override val subUserIcon: ImageView = itemView.sub_user_icon
+    override val subUserName: TextView = itemView.sub_user_name
+    override val subUserId: TextView = itemView.sub_user_id
+
+    override val reactionView: RecyclerView = itemView.reaction_view
+
+    override val replyButton: ImageButton = itemView.reply_button
+    override val reNoteButton: ImageButton = itemView.re_note_button
+    override val reactionButton: ImageButton = itemView.reaction_button
+    override val descriptionButton: ImageButton = itemView.description_button
+
+    //しっかりと実装する
+    override val mediaPlayButton: Button = Button(itemView.context)
+
+    override val reNoteCount: TextView = itemView.re_note_count
+    override val replyCount: TextView = itemView.reply_count
+    override val showThreadButton: Button = Button(itemView.context).apply { visibility = View.GONE }
+    override val elapsedTime: TextView = itemView.elapsed_time
+    override val subNoteText: TextView = itemView.sub_text
+    override val imageViewList: List<ImageView> = listOf(itemView.image_1, itemView.image_2, itemView.image_3, itemView.image_4)
+    override val mTimeFormatter: ITimeFormat = ElapsedTimeFormatter()
 
 }
--- app/src/main/java/org/panta/misskeynest/adapter/NoteDetailAdapter.kt
+++ app/src/main/java/org/panta/misskeynest/adapter/NoteDetailAdapter.kt
@@ -9,7 +9,7 @@ import org.panta.misskeynest.view_data.NoteViewData
 
 const val ITEM_TYPE_NORMAL = 0
 const val ITEM_TYPE_DETAIL = 1
-class NoteDetailAdapter(private val list: List<NoteViewData>, private val currentId: String) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class NoteDetailAdapter(private val list: List<NoteViewData>, private val currentId: String) : RecyclerView.Adapter<AbsNoteViewHolder>(){
 
     override fun getItemCount(): Int {
         return list.size
@@ -18,7 +18,7 @@ class NoteDetailAdapter(private val list: List<NoteViewData>, private val curren
     override fun getItemViewType(position: Int): Int {
         return if( list[position].id == currentId ) ITEM_TYPE_DETAIL else ITEM_TYPE_NORMAL
     }
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): AbsNoteViewHolder {
         return if( p1 == ITEM_TYPE_NORMAL ){
             val inflater = LayoutInflater.from(p0.context).inflate(R.layout.item_note, p0)
             NoteViewHolder(inflater, LinearLayoutManager(p0.context))
@@ -28,16 +28,8 @@ class NoteDetailAdapter(private val list: List<NoteViewData>, private val curren
         }
     }
 
-    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-        when(p0){
-            is NoteViewHolder ->{
-
-            }
-            is DetailNoteViewHolder ->{
-
-            }
-            else -> throw IllegalAccessException("NoteViewHolder、DetailNoteViewHolder以外を使用することはできない")
-        }
+    override fun onBindViewHolder(p0: AbsNoteViewHolder, p1: Int) {
+        p0.onBind(list[p1])
     }
 
 }
