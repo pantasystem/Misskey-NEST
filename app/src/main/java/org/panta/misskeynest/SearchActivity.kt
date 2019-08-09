940cf3e4ec74059f15377623818424a4402de4b6
kinoshita0623 <kinoshita062323@gmail.com>
2019-08-09 15:58:30 +0900

    検索画面のソフトウェアキーボードのサーチボタンを有効化した

--- app/src/main/java/org/panta/misskeynest/SearchActivity.kt
+++ app/src/main/java/org/panta/misskeynest/SearchActivity.kt
@@ -1,8 +1,12 @@
 package org.panta.misskeynest
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_search.*
 import org.panta.misskeynest.entity.ConnectionProperty
 import org.panta.misskeynest.fragment.TimelineFragment
@@ -18,7 +22,8 @@ class SearchActivity : AppCompatActivity() {
         setSupportActionBar(search_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val connectionProperty = PersonalRepository(SharedPreferenceOperator(this)).getConnectionInfo()!!
+        val connectionProperty = PersonalRepository(SharedPreferenceOperator(this))
+            .getConnectionInfo()!!
 
         val searchWord = intent.data?.getQueryParameter("searchWord")
         if(searchWord != null){
@@ -31,6 +36,18 @@ class SearchActivity : AppCompatActivity() {
             createFragment(connectionProperty, search_keyword_box.text.toString())
 
         }
+        search_keyword_box.setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH){
+
+                createFragment(connectionProperty, search_keyword_box.text.toString())
+                Log.d("", "検索開始")
+                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(search_keyword_box.windowToken, InputMethodManager.RESULT_UNCHANGED_SHOWN)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+
     }
 
     private fun createFragment(connectionProperty: ConnectionProperty, word: String){
